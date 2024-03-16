ec30cf78f9728c45465e25bb2396c4a2635934f8
Amina Saeed <aminasaeed223@gmail.com>
2024-03-16 14:49:19 +0500

    comit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// let y = 'amina\"saeed'
+
+// alert(y);
+// console.log(y);
+// Basics
+// operators
+// 5 == 5
+// this == dont compare data type
+
+// 5===5 
+// this one would compare data type as well as value
+//  != not equal data type is not compared
+//  !== data type is compared
+// not operator ! flasify every true statement
+
+
+// Conditions in javascript
+// let age = 14;
+// let hascard = "No"
+
+// if(age >= 14 && hascard == 'yes'){
+//     alert('You can vote!');
+
+// }else if(age>= 14 && hascard != 'yes'){
+//     alert('make your card')
+
+// }else{
+
+//     alert("you cant vote");
+// }
+
+
+//  if 0 then not loggedin but if 1 then loggedin
+// if (login == 0){
+//     document.write('Login')
+// }else{
+//     document.write('Logout')
+// }
+// let option = isloggedin == 1 ? "logout": "login";
+//     document.write(option)
+
+
+// Nullish Coalescing operator
+let user;
+{alert(user ?? Guest user);}
+
