d357a5bfe6e8d7e93db91d595573bd59731bb13a
Amina Saeed <aminasaeed223@gmail.com>
2024-03-19 13:33:45 +0500

    commit

---
+++ js/script01.js
@@ -0,0 +1,28 @@
+// Loops in javascript
+// let counter = 1;
+// while(counter <=10){
+//     document.write('TechGun');
+//     counter++;
+// }
+// let counter = 1;
+// sum = 0;
+// while(counter <=100){
+//     if(counter%2 == 0){
+//         sum = sum+ counter;
+//     }
+//     counter++;
+// }
+// document.write(sum);
+// do while loop
+
+// let counter = 1;
+// do{
+//     document.write("amina saeed")
+// }while(counter <=100);
+
+// for loop
+for(let counter= 1; counter<= 10; counter++){
+    document.write("TechGun")
+}
+
+
