6c09894a9daca00db17a749a3cbb9346cc7194c6
Amina Saeed <aminasaeed223@gmail.com>
2024-11-26 00:44:33 +0500

    commit

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-document.getElementById('h1head').textContent = 'hellow world'
-document.getElementById('para').textContent = 'I like pizzas'
+// let username;
+
+// document.getElementById("button").onclick = function(){
+//     username = document.getElementById("mytext").value;
+//     document.getElementById("h1").textContent = `Hello ${username}`;
+    
+// }
+
+// finding the radius of a circle
+let pi = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt('Enter radius of a circle')
+radius = Number()
+circumference = 2* pi *radius
+// console.log(circumference)
+document.getElementById("button").onclick  = circumference.value;
